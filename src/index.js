5fce04cb2ca784c317f17bc5a0dcfcf269d079b9
Dan Purcell <purcellgroup88@gmail.com>
2024-02-22 16:03:35 -0500

    dynamic port

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
